fb430b5f3be4faec57c0136eeac405938fd8160e
Feleky Mirkó <feleky@gmail.com>
2022-06-24 09:44:16 +0200

    initial commit

---
+++ precache-manifest.fa5f2e71e13b34da8cb89e95affa4eba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930d65ce01750c1b52e3654f15b827eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e42ef0999032158d92a5",
+    "url": "/static/css/main.ea78fc95.chunk.css"
+  },
+  {
+    "revision": "6f6c14c97f060426ebf7",
+    "url": "/static/js/2.cb7043b1.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.cb7043b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42ef0999032158d92a5",
+    "url": "/static/js/main.ce6dc44a.chunk.js"
+  },
+  {
+    "revision": "142b697ed77b435eaccc",
+    "url": "/static/js/runtime-main.efa8e349.js"
+  }
+]);
